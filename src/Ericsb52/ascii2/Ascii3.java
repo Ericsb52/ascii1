986adcae5addff232b963e8779f6f2c9f416797a
ebroadbent <ebroadbent@CLC-RM209-TWS.TCSD.tooelesd.org>
2018-09-10 14:09:52 -0600

    ascii art class example

---
+++ src/Ericsb52/ascii2/Ascii3.java
@@ -0,0 +1,27 @@
+package Ericsb52.ascii2;
+
+public class Ascii3 {
+
+	public static void main(String[] args) {
+		System.out.println("HH  HH     IIIIII");
+		System.out.println("HH  HH       II");
+		System.out.println("HHHHHH       II");
+		System.out.println("HH  HH       II");
+		System.out.println("HH  HH     IIIIII");
+		
+		System.out.println("--------  __@");
+		System.out.println("-----  _`\\<,_");
+		System.out.println("----  (*)/  (*)");
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~");
+		
+		
+		System.out.println(" __");
+		System.out.println("( 0>");
+		System.out.println("///\\");
+		System.out.println("\\V_/_");
+		
+		
+
+	}
+
+}
---
+++ src/Ericsb52/ascii2/Examples.java
@@ -0,0 +1,9 @@
+package Ericsb52.ascii2;
+
+public class Examples {
+
+	public static void main(String[] args) {
+		System.out.println("");
+	}
+
+}
